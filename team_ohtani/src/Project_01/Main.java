d1c5bf622b4b2d717b585876b8584b49a7131258
manic-066g <manic-066g@manic-066>
2024-04-22 11:25:34 +0900

    이진규 커밋 테스트

--- team_ohtani/src/Project_01/Main.java
+++ team_ohtani/src/Project_01/Main.java
@@ -3,5 +3,6 @@ package Project_01;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("모두 반갑습니다. 팀 오타니의 시작입니다.");
+		System.out.println("이진규 임포트 푸쉬 커밋 테스트 입니다");
 	}
 }
