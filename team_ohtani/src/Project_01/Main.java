fe55c3e26f18da619cdd61feffcabb39e58089e9
sonosung <msh071@naver.com>
2024-04-22 12:14:13 +0900

    승호 커밋

--- team_ohtani/src/Project_01/Main.java
+++ team_ohtani/src/Project_01/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("다시 텍스트를 작성해봅니다.");
 		System.out.println("이진규 인코딩 테스트");
+		System.out.println("tmdgh 인코딩 테스트");
 	}
 }
